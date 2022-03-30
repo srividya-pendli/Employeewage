559e266c868eaca9333bf202941156029776e6f8
srividya-pendli <srividyaerukala@gmail.com>
2022-03-30 18:43:51 +0530

    Add EmpolyeedailywageUC2 branch and program

---
+++ Empolyeewage/Empolyeedailywage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empolyeewage
+{
+    class Program
+    {
+        /// <summary>
+        /// Define the entry ponit of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            //UC2
+            int ispresent = 1;
+            int empRatePerHr = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+
+            //selection statement
+            if (ispresent == empInput)
+            {
+                Console.WriteLine("employee is present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("employee is Absent");
+                empHrs = 0;
+            }
+            empWage = empRatePerHr * empHrs;
+            Console.WriteLine("Daily employee wage:" + empWage);
+            Console.ReadLine();
+        }
+    }
+}
