3ee72d62d613b8987849709b659fd69b67e6105a
srividya-pendli <srividyaerukala@gmail.com>
2022-03-30 19:31:43 +0530

    Add empolyeemonthwageUC6 branch and program

--- Empolyeewage/Empolyeemonthwage.cs
+++ Empolyeewage/Empolyeemonthwage.cs
@@ -14,17 +14,22 @@ namespace Empolyeewage
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            //UC5
+            //UC6
             //consants
             int empWage = 0, empHrs = 0, totalEmpWage = 0;
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
             const int NUM_OF_WORKING_DAYS = 20;
             const int EMP_PER_HR = 20;
+            const int MAX_WORKING_HRS = 100;
+            int workingHrs = 0;
+            int day = 1;
             Random random = new Random();
-            for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
+
+            while (day <= NUM_OF_WORKING_DAYS && workingHrs <= MAX_WORKING_HRS)
             {
                 int empInput = random.Next(0, 3);
+
                 switch (empInput)
                 {
                     case FULL_TIME:
@@ -43,8 +48,15 @@ namespace Empolyeewage
                 empWage = EMP_PER_HR * empHrs;
                 Console.WriteLine("Daily wage for day{0} is:{1}", day, empWage);
                 totalEmpWage += empWage;
+                workingHrs += empHrs;
+                if (workingHrs > MAX_WORKING_HRS)
+                {
+                    workingHrs -= empHrs;
+                    break;
+                }
+                day++;
             }
-            Console.WriteLine("Total for {0}days is:{1}", NUM_OF_WORKING_DAYS, totalEmpWage);
+            Console.WriteLine("Totalwage for {0} days and working hrs:{1} and wage is:{2}", day - 1, workingHrs, totalEmpWage);
             Console.ReadLine();
         }
     }
