f938d5473c300e010c1727c0233be9b641420cdb
srividya-pendli <srividyaerukala@gmail.com>
2022-03-30 17:47:15 +0530

    Add EmpolyeewageUC1 branch and program

---
+++ Empolyeewage/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empolyeewage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1
+            int isPresent = 3;
+            Random random = new Random();
+            int empInput = random.Next(0, 4);
+
+            //selection statement
+            if (isPresent == empInput)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Empolyee Absent");
+                Console.ReadLine();
+
+        }
+    }
+}
